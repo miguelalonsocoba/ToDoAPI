6ceb67360207f9477824b44e504a82df75056343
mac <miguetheking53@gmail.com>
2022-03-22 15:54:01 -0600

    The repository is created

---
+++ src/main/java/com/mac/todoapp/persistence/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.mac.todoapp.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mac.todoapp.persistence.entity.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long>{
+
+}
